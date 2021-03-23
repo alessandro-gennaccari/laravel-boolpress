8b84ed2f45c9dfa5cf9f4c72e6bf234335cf2e20
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-23 16:18:17 +0100

    Added Faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Post;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +12,14 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 15; $i++) { 
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(3);
+            $newPost->slug = Str::slug($newPost->title);
+            $newPost->content = $faker->text(70);
+            $newPost->save();
+        }
     }
 }
