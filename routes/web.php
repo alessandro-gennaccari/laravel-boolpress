0e77222cffc46b0c7f905659e636a56365f1402b
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-25 15:56:15 +0100

    Created new routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('index');
+Route::get('/posts', 'PostController@index')->name('guest.post.index');
+Route::get('/posts/{slug}', 'PostController@show')->name('guest.post.show');
 
 Auth::routes();
 
