6322b8fb94e180b3999c26b23e1e5b98cc9f67e9
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-25 15:39:52 +0100

    changed number seeds

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -15,7 +15,7 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 15; $i++) { 
+        for ($i = 0; $i < 30; $i++) { 
 
             $newPost = new Post();
 
