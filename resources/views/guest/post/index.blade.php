f00572c4d0b32a11c85ad22ecc01a3ca41e773e1
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-25 16:26:49 +0100

    Added all post cicle

--- resources/views/guest/post/index.blade.php
+++ resources/views/guest/post/index.blade.php
@@ -3,5 +3,17 @@
 @section('title', 'Post Collections')
 
 @section('content')
-    
+<div class="container pt-5">
+
+    @foreach ($allPost as $item)
+    <div class="d-flex position-relative mb-4 p-4 bg-dark text-light border border-danger rounded">
+        <div>
+            <h5 class="mt-0">{{ $item->title }}</h5>
+            <p>{{ $item->content }}</p>
+            <a href="{{ Route('guest.post.show', $item->slug ) }}" class=" btn btn-light stretched-link">See Info</a>
+        </div>
+    </div>
+    @endforeach
+
+</div>
 @endsection
