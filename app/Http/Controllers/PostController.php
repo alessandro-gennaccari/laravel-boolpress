e98690d87363bcd618731c90eaaeb468dfa7951f
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-25 16:13:27 +0100

    Added use Model & alldata

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index() {
 
-        return view('guest.post.index');
+        return view('guest.post.index', [ 'allPost' => Post::all() ]);
 
     }
 
