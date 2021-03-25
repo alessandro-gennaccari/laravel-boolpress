c496947deceb9907ed3d2123e0cd223050909b4d
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-25 15:31:25 +0100

    Add generator user_id

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use App\Post;
+use App\User;
 
 class PostSeeder extends Seeder
 {
@@ -15,14 +16,18 @@ class PostSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i = 0; $i < 15; $i++) { 
+
             $newPost = new Post();
+
+            $userCount = count(User::all()->toArray());
+
             $newPost->title = $faker->sentence(3);
+            $newPost->user_id = rand(1, $userCount);
 
             $slug = Str::slug($newPost->title);
             $memorySlug = $slug;
             $sameSlug = Post::where('slug', $slug)->first();
             $counter = 1;
-
             while ($sameSlug) {
                 $slug = $memorySlug . '-' . $counter;
                 $sameSlug = Post::where('slug', $slug)->first();
@@ -31,6 +36,7 @@ class PostSeeder extends Seeder
             $newPost->slug = $slug;
 
             $newPost->content = $faker->text(70);
+
             $newPost->save();
         }
     }
