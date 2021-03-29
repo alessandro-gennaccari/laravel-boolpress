635c385f3dec3efa612b375ad4bafbe27735c4d8
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-29 16:00:02 +0200

    Add validaiton

--- resources/views/guest/contact.blade.php
+++ resources/views/guest/contact.blade.php
@@ -5,24 +5,39 @@
 @section('content')
 <div class="container pt-5">
 
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <form action="{{Route('guest.contact.sent')}}" method="post">
         @csrf
         @method('POST')
         <div class="form-group">
-          <label for="name">Nome</label>
-          <input type="text" class="form-control" id="name" name="name">
+            <label for="name">Nome</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name')}}">
         </div>
         <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" value="{{ old('email') }}">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
-          <label for="message">Messaggio</label>
-          <input type="textarea" class="form-control" id="message" name="message">
+            <label for="message">Messaggio</label>
+            <textarea class="form-control" id="message" name="message">{{ old('message') }}</textarea>
         </div>
         <button type="submit" class="btn btn-primary">Invia</button>
-      </form>
+    </form>
 
 </div>
 @endsection
